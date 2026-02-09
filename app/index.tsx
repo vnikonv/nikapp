69d7288e275be1e8f20e3c04caaf3f3bde2a0d5f
Nikon Veremeichik <thesecondnikon@gmail.com>
2026-02-09 16:30:01 +0500

    Working nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-center text-3xl text-green-500">This stuff has been edited.</Text>
     </View>
   );
 }
