37b0c6f0a8384c41e0150e4e4440fb2d2ee6b1cd
Nikon Veremeichik <thesecondnikon@gmail.com>
2026-03-02 18:04:58 +0500

    Add custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "./global.css";
+import { useFonts } from "expo-font";
+import { use, useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    FreePixel: "https://sadhost.neocities.org/fonts/FreePixel.ttf",
+  });
+
+  useEffect(() => {
+    if (error) {
+      throw error;
+    }
+    if (!fontsLoaded) {
+      SplashScreen.hideAsync;
+    }
+  }, [fontsLoaded, error]);
+
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-center text-3xl text-green-500">This stuff has been edited.</Text>
+      <Text className="text-center text-3xl text-green-500 font-freepixel">
+        This stuff has been edited.</Text>
     </View>
   );
 }
